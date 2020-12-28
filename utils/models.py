fdc21f615868842c44469b9538ccbbe2f65f93d3
John Hawkins <hawkins.john.c@gmail.com>
2020-12-28 02:55:13 +0000

    Working through bugs and experimenting with SKLearn Pipelines

--- utils/models.py
+++ utils/models.py
@@ -27,7 +27,15 @@ def register(name, description, artefact, endpoint):
     """
       Register a model for comparison
     """
-    temp = {"name": name, "description":description, "artefact":artefact, "endpoint":endpoint}
+    # We need the endpoint name and class in order to re-create an endpoint
+    endpoint_name = endpoint._endpoint_config_name
+    endpoint_class = endpoint.__class__
+    temp = {"name": name, 
+            "description":description,
+            "artefact":artefact, 
+            "endpoint_name":endpoint_name,
+            "endpoint_class":endpoint_class
+    }
     config = get_config()
     config['models'][name] = temp
     write_config(config)
